166b50023bb55476ae9ecc0d9c58796a6341a848
memchenko <emchenko.mikhail@mail.ru>
2021-09-17 17:39:56 +0400

    fix exports

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
-from util.extract_feature import extract_feature
-from backbone.model_irse import IR_50
-from align.detector import detect_faces
-from align.visualization_utils import show_results
-from align.face_align import align
-from parsing.face_parsing import parsing, vis_parsing_maps
+from . import util
+from .util.extract_feature import extract_feature
+
+from . import backbone
+from .backbone.model_irse import IR_50
+
+from . import align
+from .align.detector import detect_faces
+from .align.visualization_utils import show_results
+from .align.face_align import align
+
+from . import parsing
+from .parsing.face_parsing import parsing, vis_parsing_maps
